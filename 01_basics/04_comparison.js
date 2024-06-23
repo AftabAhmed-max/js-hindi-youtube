6424f4e774d40f26cbbe426c72218aea14c152b9
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-23 18:00:36 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true // avoid thes types of comparisons and same for undefined too
+
+// === checks the values and the data type too 
+// == checks only the value
