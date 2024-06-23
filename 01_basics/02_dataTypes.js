45bdf6321e3870e968ca3908d12747a5bc96fd07
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-23 16:07:50 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version not compulsion but if done well and good
+
+//alert(3+3) using node js this command wont work it will work in browser
+
+let name = "aftab"; //string
+let age = 18 ; //number
+let isLoggedIn = false; //boolean
+
+//number => 2 to the power 53 is max //will use bigInt for more than this
+//string => ""
+// boolean => true/false
+// null => its a standalone value (null ka type will be object)
+// undefined => 
+// symbol => used for uniqueness
+
+//object 
+
+console.log(typeof "Aftab");
+console.log(typeof undefined);
+console.log(typeof null);
