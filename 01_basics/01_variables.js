4d1bbda1bff66105042fea8d8659737add008703
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-23 15:48:23 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,17 @@
-const accoundId = 12345
-let accountEmail = "aftab@gmail.com"
-var accountPassword = "122456"
-accountCity = 'Jaipur'
+const accountId = 12345;
+let accountEmail = "aftab@gmail.com";
+let accountState; // this is one way to define just the variable withh no value its value will be undefined
+var accountPassword = "122456";
+accountCity = 'Jaipur' //js auto store the variable but dont it this way always defie with const and let
 
-// accoundId = 2 
 
-console.log(accoundId);
+
+// accountId = 2 
+
+accountEmail = "ahmed@gmail.com";
+accountPassword  = "212121"
+accountCity = "Mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//never use var because of issue in block scope and functional scope 
+// console.log(accountId);
