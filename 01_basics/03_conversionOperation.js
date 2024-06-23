133d5f79da39c66079965f86d8012d6e469e0adf
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-23 16:55:10 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
-let score = "33abc";
+// let score = "33abc";
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber); 
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);
 
 //for playing number js will give datatype as number only but if you include some alphabets with number it ma sometime still give the datatype as number only where the value of the variable can be NaN
 
@@ -12,11 +12,48 @@ console.log(valueInNumber);
 // "33abc" => Nan
 // true => 1; false => 0
 
-let isLoggedIn = 1;
-console.log(isLoggedIn);
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1;
+// console.log(isLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // "" => false;
 // "aftab" => true;
 
+/*********************************operations*********************************/
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// let str1 = "Hello";
+// let str2 = " Aftab";
+// console.log(str1 + str2);
+
+// console.log(1 + "3");
+// console.log("3" + 1);
+// console.log( 3 + 2 + "1");
+// console.log( "3" + 2 + 1); // dont be dependent on adding number and string together always convert both to one datatype before adding as per the output that you want
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+
+/************** yeh ho gaya Post Fix ********************/
+// let x = 3; 
+// const y = x++;
+// // x is 4; y is 3 so yahan postfix mein yeh x ki value toh badha dega lekin y mei yeh x ki jo purani value hai wahi dalega
+
+// let x2 = 23;
+// let y2 = x2++;
+// console.log(x2);
+// console.log(y2);
+
+/************** yeh ho gaya Pre Fix ********************/
+
+// let x2 = 23;
+// let y2 = ++x2;
+// console.log(x2);
+// console.log(y2);
+
+//yahan prefix mei yeh aisa hota hai ki x2 ki value badhakr dega and uske ssath jo x ki jo nayi value hai ausi ko y mei bhi daal dega
