83f71b2def3f238c9c3e348dcb9ef1add4e7b30c
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-25 17:49:48 +0000

    arrays 1 done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,22 @@
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+// console.log(typeof myDate); //type off date will be object
+
+// let myNewDate = new Date("01-23-2012");
+// console.log(myNewDate.getTime());
+// // console.log(myNewDate.toDateString());
+
+// let timeStamp = Date.now();
+// console.log(timeStamp.getTime());
+// console.log(Math.floor(Date.now()/1000)); 
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getFullYear());
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,47 @@
+// const myArray = [0,1,2,3,4,5,true,"Aftab"]; // js arrays are resizable
+// console.log(myArray[7]); //it follows the HEAP memory system
+
+//different ways to define an array
+// let names =["aftab","ahmed"];
+// let name = new Array(1,2,3,4,5);
+
+//Array Methods
+
+// name.push(6); // adds element at the end of the array
+// console.log(name);
+// name.pop(); // removes element from the end of the array
+// console.log(name);
+// name.unshift(10); // adds element at the start of the array
+// console.log(name);
+// name.shift(); // removes element from the start of the array
+// console.log(name);
+
+
+// console.log(name.includes(19)); // to check any value inside the array
+// console.log(name.indexOf(3)); // to check the index of the value in the array
+
+// let newName = name.join(); //converts the array into string
+// console.log(newName);
+
+//slice:-
+//1. slice doesn't make any changes in the original array 
+//2. it will give a new array from the starting index and will end before the ending index without including the ending index
+
+//Splice
+//1. splice makes changes in the original array
+//2. it will give a new array from the starting index and will include the ending index 
+
+// let array1 = [1,2,3,4,5];
+// console.log(array1);
+
+// let array2 = array1.slice(1,4);
+// console.log(array2);
+// console.log(array1);
+
+// let array11 = [1,2,3,4,5,6,7,8,9];
+// console.log(array11);
+
+// let array12 = array11.splice(1,5);
+// console.log(array12);
+// console.log(array11);
+
