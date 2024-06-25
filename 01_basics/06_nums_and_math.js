277ddbbfad03fa7ca544c4e8635f53a9938fa164
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-25 08:27:26 +0000

    NUM num and math done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+// let balance = 100;
+// console.log(balance);
+
+// let sco = new Number(300);
+// console.log(sco);
+
+// console.log(sco.toString().length);
+// console.log(sco.toFixed(2)); // to get limited number after the decimal point
+// let other = 10000000;
+// console.log(other.toPrecision(3));
+// console.log(other.toLocaleString('en-IN'));
+
+
+/**************************************Maths****************************************/
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //negative to positive
+
+// console.log(Math.round(28.345435));
+// console.log(Math.ceil(28.345435));
+// console.log(Math.floor(28.647643));
+// console.log(Math.min(2,4,5,1));
+// console.log(Math.max(4,5,4,546,3345,));
+
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1);
+// console.log(Math.floor(Math.random()*10 + 1));
+let min= 10;
+let max = 20;
+let num = console.log(Math.floor(Math.random()*(max-min + 1) + min)); 
