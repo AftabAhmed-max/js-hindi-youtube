69aed733ba5b08d49781eef6cd18d1904c99b324
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-25 05:56:04 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+// const name = "Aftab";
+// const count = 25;
+// console.log(name + count + 2024);
+// console.log(`Hello my name is ${name} and my repo count is ${count} and this is year 2024`);
+// this is called string interpolation
+
+const vc = new String('aftabahmed');
+
+// console.log(vc[0]);
+// console.log(vc.__proto__);
+// console.log(vc.length);
+// console.log(vc.toUpperCase());
+// console.log(vc.charAt(4));
+// console.log(vc.indexOf('t'));
+
+// let newName = vc.substring(0,4);
+// console.log(newName);
+
+// let new2Name = vc.slice(-8,4);
+// console.log(new2Name);
+
+// const new3NAme = "   aftab   ";
+// console.log(new3NAme);
+// console.log(new3NAme.trim());
+
+// let name = 'Aftab Ahmed Gauhar Ali'
+// console.log(name.replace(' ','-'));
+// console.log(name.includes('Aftaba'));
+
+// console.log(name.split(' '));
+
