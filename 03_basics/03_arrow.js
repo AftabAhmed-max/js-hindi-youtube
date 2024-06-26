0c5de58f1f7fb02cafa544b805dc17bb46d4c6f3
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-26 20:21:51 +0000

    arrow func

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,31 @@
+// let user = {
+//     username : "Aftab",
+//     price : 999,
+//     welcomeMessage : function() {
+//         console.log(`${this.username} , welcome to the website`);
+//         console.log(this); // whatever we have inside this object it will rpint that
+//     }
+// }
+// user.welcomeMessage();
+
+// this can only be used inside an object and not inside a normal function either it will give a lot of values or undefined
+
+
+// normal fucntion declaration 
+// function chai() {
+//     console.log("Hello World");
+// }
+// chai();
+
+
+//basic arrow function declaration (explicit return)
+// let chai = () => {
+//     return "Hello World";
+// }
+// chai();
+
+// implicit return arrow function 
+//(here if there is return inside {} and single line only then we can remove return and {}
+// we can also wrap the material after arrow in parenthesis highly used for objects
+let chai = (num1,num2) => num1 + num2;
+console.log(chai(4,8));
