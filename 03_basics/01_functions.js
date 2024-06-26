83e48819e2eac6bc97a0d174b953bd75e8f36507
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-26 18:13:50 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,9 +28,36 @@
 
 
 // using rest operator when you dont know the number of arguements you are going to pass it is same as spread operator
-function calculate(...num1) {
-    return num1;
+// function calculate(...num1) {
+//     return num1;
+// }
+// console.log(calculate(200,300,400));
+
+
+// taking an object inside a function 
+
+// let user = {
+//     name : "Aftab",
+//     price : 199,
+// }
+
+// function obj1(anyobj) {
+//     console.log(`name is ${anyobj.name} and price is ${anyobj.price}`);
+// }
+
+// obj1(user) // here if we change the value of any key of the object user the output will be undefined
+
+// taking an array inside a function 
+
+let myArray = [100,300,500,700,900];
+
+function returnArray(newArray) {
+    return newArray[1];    
 }
-console.log(calculate(200,300,400));
+
+console.log(returnArray(myArray));
+
+
+
 
 
