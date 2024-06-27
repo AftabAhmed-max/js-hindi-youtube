1a2ecfe2957d4d2f80d02dcdd5846923c0954cd2
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-27 08:56:26 +0000

    for_loop

---
+++ 05_iterations/01_one.js
@@ -0,0 +1,43 @@
+// for loop
+
+// for (let i = 0 ; i <= 10 ; i++ ) {
+//     let number = i ;
+//     if (number == 5) {
+//         console.log("5 is worst number");
+//     }
+//     console.log(number);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+    // console.log(`i = ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+
+// let myArray = ["Aftab","Ahmed","Gauhar","Ali"];
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element)
+// }
+
+// break (once the break keyword enters the loop ends then and there)
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("5 is here");
+//         break;
+//     }
+//     console.log(`Value of I is ${i}`);
+// }
+
+// Continue (in continue keyword the loop skips the link on which the continue statement is applied)
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("5 is here");
+//         continue;
+//     }
+//     console.log(`Value of I is ${i}`);
+// }
