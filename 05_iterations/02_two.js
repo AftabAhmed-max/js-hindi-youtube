dae310b482f28ac8ff4c76e8e97a8a795d336d66
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-27 16:49:25 +0000

    iteration

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,22 @@
+// while Loop
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(i);
+//     i = i + 2;
+// }
+
+// let myArray = ["Aftab","Ahmed","Ali"];
+// let i = 0;
+// while (i < myArray.length) {
+//     console.log(myArray[i]);
+//     i++;
+// }
+
+// Do while Loop (this will execute atleast once m )
+
+let score = 1 ;
+do {
+    console.log(score);
+    score++ ;
+} while (score = 0);
