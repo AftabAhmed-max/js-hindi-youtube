394a45a983d4e45e1c3e57b5e56dc524516b4321
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-27 07:09:08 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,20 @@
+// switch case is just like IF eelse statement does the same work just syntax little bit different 
+
+let month = 7;
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("Some Other Month");
+        break;
+}
