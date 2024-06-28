47ed8922cee4ec9078e080f4b29351bf898b4f6b
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-28 14:41:36 +0000

    done

---
+++ 05_iterations/04_for_in.js
@@ -0,0 +1,33 @@
+// For in loop
+
+let myObj = {
+    js : "javaScript",
+    cpp : "c++",
+    rb : "ruby",
+    swift : "swift by apple"
+}
+
+// for (const key in myObj) {
+//     console.log(key);
+// } // this will print only keys of the object
+
+// for (const key in myObj) {
+//     console.log(myObj[key]);
+// } // this will print only values of keys of the object
+
+// for (const key in myObj) {
+//     console.log(`${key} : ${myObj[key]}`);
+// } // this syntax will print key:value pair of the object
+
+// for in loop for Arrays
+// let programming = ["js","cpp","ruby","java"]
+// for (const key in programming) {
+//     console.log(key);
+// } // this will print only the index of the array
+
+// let programming = ["js","cpp","ruby","java"]
+// for (const key in programming) {
+//     console.log(programming[key]);
+// } // this will print the value of the array {if you want value with index number you can use ``}
+
+// For in loop cannot be used on Map 
