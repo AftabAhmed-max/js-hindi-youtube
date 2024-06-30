438b18df88bdd73bf8ea58a3dc6d42e4f24ac523
AftabAhmed-max <57930301+AftabAhmed-max@users.noreply.github.com>
2024-06-30 05:29:46 +0000

    proj 1

--- 07_projects/01_color_changer/chaiaurcode.js
+++ 07_projects/01_color_changer/chaiaurcode.js
@@ -1,2 +1,40 @@
 let buttons = document.querySelectorAll('.button');
-console.log(buttons);
+let body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
+
+//   colorChange('yellow');
+//   colorChange('blue');
+//   colorChange('grey');
+//   colorChange('white');
+
+//  function colorChange(color) {
+//   buttons.forEach( function(button) {
+//     console.log(button);
+//     button.addEventListener('click', function(e) {
+//       console.log(e)
+//       console.log(e.target)
+//       if(e.target.id === color) {
+//        body.style.backgroundColor = e.target.id;
+//       }
+//     });
+//   });
+//  }
